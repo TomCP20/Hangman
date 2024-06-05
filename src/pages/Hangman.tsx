4c55e1310a19351e86a42c3ef1a0ae6e23a0d0ea
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-06-05 22:18:41 +0100

    switched from flex justify centre to text centre

--- src/pages/Hangman.tsx
+++ src/pages/Hangman.tsx
@@ -55,7 +55,7 @@ export default function Hangman() {
 
       <InputChar handleClick={handleClick} guessed={guessed} gameOver={gameOver} />
 
-      <div className="flex justify-center">
+      <div className="text-center">
         <button onClick={handleReset} className='text-center font-mono text-2xl m-1 p-1 h-12 w-24 rounded-md border border-solid border-black bg-gray-200 hover:bg-gray-300'>Reset</button>
         <Link to={"/"}><button type='button' className='text-center font-mono text-2xl m-1 p-1 h-12 w-24 rounded-md border border-solid border-black bg-gray-200 hover:bg-gray-300'>Home</button></Link>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export function Home() {
   return (
     <>
       <Diagram stage={6} />
-      <div className="flex justify-center">
+      <div className="text-center">
         <Link to={"game"}>
           <button type='button' className="text-center font-mono text-2xl m-1 p-1 h-12 w-24 rounded-md border border-solid border-black bg-gray-200 hover:bg-gray-300">Play!</button>
         </Link>
